a19ad8ba84ce8fe3a2dfbd5802b9e9a4a07eb97a
JunHwan Kim <jjun940417@gmail.com>
2025-02-26 15:38:36 +0900

    add composables and functions

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.artspace
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
@@ -23,21 +18,23 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,6 +53,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val artworkIdxs = listOf(0, 1, 2)
 @Composable
 fun ArtSpaceLayout() {
     Column(
@@ -63,35 +61,100 @@ fun ArtSpaceLayout() {
             .padding(20.dp)
             .safeDrawingPadding(),
     ) {
-        Surface(modifier = Modifier.wrapContentSize(), shadowElevation = 5.dp) {
-            Image(
-                modifier = Modifier.fillMaxWidth()
-                    .padding(20.dp),
-                painter = painterResource(R.drawable.img),
-                contentDescription = "sunflower"
-            )
-        }
+        var currentArtwork by remember { mutableIntStateOf(0) }
+        Artwork(currentArtwork)
         Spacer(modifier = Modifier.size(15.dp))
-        Column(
-            modifier = Modifier.fillMaxWidth().background(color = Color.LightGray)
-                .padding(vertical = 10.dp, horizontal = 5.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Artwork Title", style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
-            Text(text = "Artwork Artist (Year)", style = MaterialTheme.typography.titleSmall, textAlign = TextAlign.Center)
-        }
+        ArtDescription(currentArtwork)
         Spacer(modifier = Modifier.size(15.dp))
-        Row(
+        BottomButtons(
+            onClickPrev = {
+                if(currentArtwork == 0) {
+                    currentArtwork = 2
+                } else {
+                    currentArtwork -= 1
+                }
+            },
+            onClickNext = {
+                if(currentArtwork == 2) {
+                    currentArtwork = 0
+                } else {
+                    currentArtwork += 1
+                }
+            }
+        )
+    }
+}
+@Composable
+fun Artwork(currentArtwork: Int) {
+    val drawable = when(currentArtwork) {
+        artworkIdxs[0] -> R.drawable.art_0
+        artworkIdxs[1] -> R.drawable.art_1
+        artworkIdxs[2] -> R.drawable.art_2
+        else -> R.drawable.art_0
+    }
+    Surface(modifier = Modifier.wrapContentSize(), shadowElevation = 5.dp) {
+        Image(
             modifier = Modifier.fillMaxWidth()
-                .padding(vertical = 5.dp, horizontal = 10.dp),
+                .padding(20.dp),
+            painter = painterResource(drawable),
+            contentDescription = "sunflower"
+        )
+    }
+}
+
+@Composable
+fun ArtDescription(currentArtwork: Int) {
+    val titleRes = when(currentArtwork) {
+        artworkIdxs[0] -> R.string.title_art_0
+        artworkIdxs[1] -> R.string.title_art_1
+        artworkIdxs[2] -> R.string.title_art_2
+        else -> R.string.title_art_0
+    }
+    val artistRes = when(currentArtwork) {
+        artworkIdxs[0] -> R.string.artist_art_0
+        artworkIdxs[1] -> R.string.artist_art_1
+        artworkIdxs[2] -> R.string.artist_art_2
+        else -> R.string.artist_art_0
+    }
+    Column(
+        modifier = Modifier.fillMaxWidth().background(color = Color.LightGray)
+            .padding(vertical = 10.dp, horizontal = 5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(titleRes),
+            style = MaterialTheme.typography.titleLarge,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(artistRes),
+            style = MaterialTheme.typography.titleSmall,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun BottomButtons(onClickPrev: () -> Unit, onClickNext: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(vertical = 5.dp, horizontal = 10.dp),
+    ) {
+        Button(
+            modifier = Modifier.fillMaxWidth().weight(1.0f),
+            onClick = onClickPrev
         ) {
-            Button(modifier = Modifier.fillMaxWidth().weight(1.0f), onClick = { Log.d("Button", "clicked") }) {
-                Text(stringResource(id = R.string.previous))
-            }
-            Spacer(modifier = Modifier.size(20.dp))
-            Button(modifier = Modifier.fillMaxWidth().weight(1.0f), onClick = { Log.d("Button", "clicked") }) {
-                Text(stringResource(id = R.string.next))
-            }
+            Text(stringResource(id = R.string.previous))
+        }
+        Spacer(modifier = Modifier.size(20.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth().weight(1.0f),
+            onClick = onClickNext
+        ) {
+            Text(stringResource(id = R.string.next))
         }
     }
 }
